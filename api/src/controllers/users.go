7a05805f7cab724357efd18aeeb4567463483daf
Vladislav Kasperovich <vladislav.kasperovich02@gmail.com>
2026-04-22 11:34:43 +0100

    branch rebase

--- api/src/controllers/users.go
+++ api/src/controllers/users.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/GerundiumLovecraft/team-hot-chip-group-project/api/src/auth"
 	"github.com/GerundiumLovecraft/team-hot-chip-group-project/api/src/models"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
@@ -39,25 +41,52 @@ func CreateUser(ctx *gin.Context) {
 }
 
 func GetUserById(ctx *gin.Context) {
-	id := ctx.Param("id")
+	// Get the user ID from the token
+	userId, exists := ctx.Get("userID")
+	userIdStr, ok := userId.(string)
 
-	user, err := models.FindUser(id)
+	// Get the user ID from the parameters
+	searchedId := ctx.Param("id")
 
-	if err != nil {
-		ctx.JSON(http.StatusNotFound, gin.H{"message": "User not found"})
+	// Check if the user's ID matches to the searched ID
+	if !exists || !ok || userIdStr != searchedId {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"message": "Unauthorised access to user's profile"})
 		return
 	}
-	ctx.JSON(http.StatusOK, user)
+
+	// Find the use by ID
+	user, err := models.FindUser(searchedId)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"message": "User with this ID not found"})
+		return
+	}
+
+	// Generate new token
+	token, _ := auth.GenerateToken(userIdStr)
+
+	// Send the response
+	ctx.JSON(http.StatusOK, gin.H{"message": "OK", "user": user, "token": token})
 }
 
 func GetUserByUsername(ctx *gin.Context) {
+	// Search for the user by the username
 	username := ctx.Param("username")
-
 	user, err := models.FindUserByUsername(username)
-
 	if err != nil {
 		ctx.JSON(http.StatusNotFound, gin.H{"message": "Username not found"})
 		return
 	}
-	ctx.JSON(http.StatusOK, user)
+
+	// To keep the page private for now, check if the user's ID from the token matches with the ID in user object
+	userId, exists := ctx.Get("userID")
+	userIdStr, ok := userId.(string)
+	uIntToStr := strconv.FormatUint(uint64(user.ID), 10)
+	if !exists || !ok || userIdStr != uIntToStr {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"message": "Unauthorised access to user's profile"})
+		return
+	}
+	// Generate new token
+	token, _ := auth.GenerateToken(userIdStr)
+	// Send the response
+	ctx.JSON(http.StatusOK, gin.H{"message": "OK", "user": user, "token": token})
 }
