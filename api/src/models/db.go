51dbbe4bc4ac128d2df590de8d4cff6f70fc5fd3
Jack Garner <jackgarner@Jacks-MacBook-Pro.local>
2026-04-24 16:43:15 +0100

    Removed legacy acebook code.

--- api/src/models/db.go
+++ api/src/models/db.go
@@ -26,7 +26,6 @@ func OpenDatabaseConnection() {
 
 func AutoMigrateModels() {
 	Database.AutoMigrate(&User{})
-	Database.AutoMigrate(&Post{})
 	Database.AutoMigrate(&Spot{})
 	Database.AutoMigrate(&Feature{})
 	Database.AutoMigrate(&SpotsToFeats{})
