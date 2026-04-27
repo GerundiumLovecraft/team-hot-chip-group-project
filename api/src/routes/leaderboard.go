ee5c3531a84c9cfe84c1ce2372d2ff39285d36c0
s-arahmaria <sarahmaria12345@hotmail.co.uk>
2026-04-27 17:37:45 +0100

    add leaderboard page with podium layout and make endpoint private

--- api/src/routes/leaderboard.go
+++ api/src/routes/leaderboard.go
@@ -2,10 +2,11 @@ package routes
 
 import (
 	"github.com/GerundiumLovecraft/team-hot-chip-group-project/api/src/controllers"
+	"github.com/GerundiumLovecraft/team-hot-chip-group-project/api/src/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func setupLeaderboardRoutes(baseRouter *gin.RouterGroup) {
 	leaderboard := baseRouter.Group("/leaderboard")
-	leaderboard.GET("", controllers.GetLeaderboardData)
+	leaderboard.GET("", middleware.AuthenticationMiddleware, controllers.GetLeaderboardData)
 }
