b4e6b52e80b9c37ec4f37b26f10cc243e946d6ab
Wedna Guirand <wedna.guirand@gmail.com>
2026-04-27 23:56:07 +0100

    added the get all feature to the service feature file

--- frontend/src/services/features.js
+++ frontend/src/services/features.js
@@ -8,12 +8,32 @@ export const getAllFeatures = async () => {
         },
     };
 
-    const response = await fetch(`${BACKEND_URL}/spots/features`, requestOptions);
+    const response = await fetch(`${BACKEND_URL}/features`, requestOptions);
 
     if (response.status !== 200) {
         throw new Error("Unable to fetch all features!");
     }
 
     const data = await response.json();
+    return data;
+};
+
+export const getFeatures = async () => {
+
+    const requestOptions = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+
+    const response = await fetch(`${BACKEND_URL}/features`, requestOptions);
+
+    if (!response.ok) {
+        throw new Error("Server issue. Try again later!");
+    }
+
+    const data = await response.json();
+
     return data;
 };
