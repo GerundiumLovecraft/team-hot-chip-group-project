aae28bfb7c81a05574b027e9a751baf53a7b17c2
s-arahmaria <sarahmaria12345@hotmail.co.uk>
2026-04-29 11:11:37 +0100

    add global styles, fonts, colour variables and clean up all css files

--- frontend/src/pages/Signup/SignupPage.jsx
+++ frontend/src/pages/Signup/SignupPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-
+import "./SignupPage.css";
 import { signup } from "../../services/authentication";
 
 export const SignupPage = () => {
