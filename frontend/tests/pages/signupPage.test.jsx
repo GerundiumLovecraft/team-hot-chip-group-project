c0c6f5143d2bba860d1cba5b6c11c1e09a1759e7
Wedna Guirand <wedna.guirand@gmail.com>
2026-05-01 09:49:48 +0100

    refactor the test for sign up

--- frontend/tests/pages/signupPage.test.jsx
+++ frontend/tests/pages/signupPage.test.jsx
@@ -1,18 +1,29 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { vi } from "vitest";
+import { afterEach, vi } from "vitest";
 
-import { useNavigate } from "react-router-dom";
+import { MemoryRouter, useNavigate } from "react-router-dom";
 import { signup } from "../../src/services/authentication";
 
 import { SignupPage } from "../../src/pages/Signup/SignupPage";
 
+const navigateMock = vi.fn();
+
 // Mocking React Router's useNavigate function
-vi.mock("react-router-dom", () => {
-  const navigateMock = vi.fn();
-  const useNavigateMock = () => navigateMock; // Create a mock function for useNavigate
-  return { useNavigate: useNavigateMock };
+vi.mock("react-router-dom", async (importOriginal) => {
+  const original = await importOriginal();
+  return {... original, useNavigate: () => navigateMock };
 });
+
+// Mocking localStorage
+const localStorageMock = {
+  setItem: vi.fn(),
+  getItem: vi.fn(),
+  removeItem: vi.fn(),
+  clear: vi.fn()
+}
+Object.defineProperty(window, "localStorage", { value: localStorageMock })
+
 
 // Mocking the signup service
 vi.mock("../../src/services/authentication", () => {
@@ -24,46 +35,53 @@ vi.mock("../../src/services/authentication", () => {
 const completeSignupForm = async () => {
   const user = userEvent.setup();
 
-  const emailInputEl = screen.getByLabelText("Email:");
-  const passwordInputEl = screen.getByLabelText("Password:");
-  const submitButtonEl = screen.getByRole("submit-button");
+  const usernameInputEl = screen.getByPlaceholderText("Username");
+  const emailInputEl = screen.getByPlaceholderText("Email address");
+  const passwordInputEl = screen.getByPlaceholderText("Create a password");
+  const confirmPasswordInputEl = screen.getByPlaceholderText("Confirm password");
+  const loginButtonEl = screen.getByRole("button", {name: /sign up/i});
 
+  await user.type(usernameInputEl, "tester")
   await user.type(emailInputEl, "test@email.com");
-  await user.type(passwordInputEl, "1234");
-  await user.click(submitButtonEl);
+  await user.type(passwordInputEl, "password1234");
+  await user.type(confirmPasswordInputEl, "password1234")
+  await user.click(loginButtonEl);
 };
 
 describe("Signup Page", () => {
-  beforeEach(() => {
+  afterEach(() => {
     vi.resetAllMocks();
   });
 
   test("allows a user to signup", async () => {
-    render(<SignupPage />);
+    render(<MemoryRouter><SignupPage /></MemoryRouter>);
 
     await completeSignupForm();
 
-    expect(signup).toHaveBeenCalledWith("test@email.com", "1234");
+    expect(signup).toHaveBeenCalledWith("tester", "test@email.com", "password1234", "password1234");
   });
 
-  test("navigates to /login on successful signup", async () => {
-    render(<SignupPage />);
-
-    const navigateMock = useNavigate();
+  test("navigates to / on successful signup", async () => {
+    vi.spyOn(Storage.prototype, "setItem").mockImplementation(() => {});
+    signup.mockResolvedValue({token: "secrettoken123"});
+    render(<MemoryRouter><SignupPage /></MemoryRouter>);
 
     await completeSignupForm();
 
-    expect(navigateMock).toHaveBeenCalledWith("/login");
+    await waitFor(() => {
+      expect(navigateMock).toHaveBeenCalledWith("/");
+    })
   });
 
-  test("navigates to /signup on unsuccessful signup", async () => {
-    render(<SignupPage />);
-
+  test("shows an error message when the user is unsuccessful in loging in", async () => {
+    vi.spyOn(Storage.prototype, "setItem").mockImplementation(() => {});
     signup.mockRejectedValue(new Error("Error signing up"));
-    const navigateMock = useNavigate();
+    render(<MemoryRouter><SignupPage /></MemoryRouter>);
 
     await completeSignupForm();
 
-    expect(navigateMock).toHaveBeenCalledWith("/signup");
+    await waitFor(() => {
+      expect(screen.getByText("Error signing up")).toBeTruthy();
+    })
   });
 });
